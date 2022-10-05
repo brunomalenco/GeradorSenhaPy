313c38c5b84b0e3d4d1de4396545722516d81740
brunomalenco <bruno8ian@hotmail.com>
2022-10-05 16:09:39 -0300

    teste

---
+++ Projeto/MiniFramework/App/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App\Controllers;
+
+    class IndexController{
+
+        public function index(){
+            echo "Chegamos ao indexController";
+        }
+
+        public function sobreNos(){
+            echo "Chegamos ao SOBRENOS";
+        }
+    }
+
+?>
---
+++ Projeto/MiniFramework/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mini/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1ce58f7746ecc155f7370908c377d7b451c9d3c1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mini/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1ce58f7746ecc155f7370908c377d7b451c9d3c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
