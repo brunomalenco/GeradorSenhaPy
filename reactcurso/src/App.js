3cf86bcff568337f3e4aa76689f00d41725abcec
brunomalenco <bruno8ian@hotmail.com>
2022-12-12 01:21:21 -0300

    teste

--- reactcurso/src/App.js
+++ reactcurso/src/App.js
@@ -3,17 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    posts: []
+  }
+
+
+  componentDidMount() {
+    this.loadPosts()
+  }
+
+  loadPosts = async () => {
+    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts')
+    const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+
+    const [posts, photos] = await Promise.all([postResponse, photoResponse])
+
+    const postJson = await posts.json()
+    const photosJson = await photos.json()
+
+    const postsAndPhotos = postJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url}
+    })
+
+    this.setState({ posts: postsAndPhotos })
+  }
+
   render() {
+    const { posts } = this.state
+    //const name = this.state.name
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <section className='container'>
+        <div className="posts">
+          {posts.map(posts => (
+            <div className='post'>
+              <img src={posts.cover} alt={posts.title}></img>
+              <div className='post-content' key={posts.id}>
+                <h2>{posts.title}</h2>
+                <p>{posts.body}</p>
+              </div>
+            </div>
+          ))}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </section>
     );
   }
 }
